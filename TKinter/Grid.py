6975107976625f3bc484f4d3da2327fb3417e825
temple2430 <armygrad24@gmail.com>
2019-12-29 20:13:02 +0000

    Commit

--- TKinter/Grid.py
+++ TKinter/Grid.py
@@ -2,9 +2,11 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root,text="Hello World")
+myLabel1 = Label(root,text="Hello World")
+myLabel2 = Label(root,text="Hello World")
 
-myLabel.pack()
+myLabel1.grid(row=0,column=0)
+myLabel2.grid(row=5,column=0)
 
 root.mainloop()
 
