752e1b261a370ac37f2c7fd8c89142476d40a4da
temple2430 <armygrad24@gmail.com>
2019-12-30 01:06:50 +0000

    Commit

--- TKinter/Entry.py
+++ TKinter/Entry.py
@@ -2,12 +2,17 @@ from tkinter import *
 
 root = Tk()
 
+e = Entry(root,borderwidth=5)
+e.pack()
+e.insert(0, "Enter your Name")
+
 def myClick():
-    myLabel = Label(root,text="I clicked the Button")
+    Hello = "Hello " + e.get()
+    myLabel = Label(root,text=Hello)
     myLabel.pack()
 
 #myButton = Button(root, text = "Click Me", padx=20, pady=20, command=myClick, fg="purple")
-myButton = Button(root, text = "Click Me", command=myClick, fg="purple", bg="yellow")
+myButton = Button(root, text = "Enter your name", command=myClick, fg="purple", bg="yellow")
 myButton.pack()
 
 root.mainloop()
