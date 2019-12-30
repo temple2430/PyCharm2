a9d1bc3da250cd77bec720575fad017df0005ca0
temple2430 <armygrad24@gmail.com>
2019-12-30 20:19:20 +0000

    Commit

--- TKinter/Database.py
+++ TKinter/Database.py
@@ -2,6 +2,9 @@ from tkinter import *
 import sqlite3
 
 root = Tk()
+root.title('Create Entry')
+root.geometry("400x600")
+
 conn = sqlite3.connect('address_book.db')
 cur = conn.cursor()
 
@@ -13,6 +16,55 @@ cur = conn.cursor()
 #                state text,
  #               post_code integer)
  #           """)
+def edit():
+    editor = Tk()
+    editor.title('Update a Record')
+    editor.geometry("400x600")
+
+    conn = sqlite3.connect('address_book.db')
+    cur = conn.cursor()
+    record_id = delete_box.get()
+
+    cur.execute("SELECT * FROM addresses WHERE oid = " + record_id)
+    records = cur.fetchall()
+
+    f_name_editor = Entry(editor, width=30)
+    f_name_editor.grid(row=0, column=1, padx=20, pady=(10, 0))
+    l_name_editor = Entry(editor, width=30)
+    l_name_editor.grid(row=1, column=1)
+    address_editor = Entry(editor, width=30)
+    address_editor.grid(row=2, column=1)
+    city_editor = Entry(editor, width=30)
+    city_editor.grid(row=3, column=1)
+    state_editor = Entry(editor, width=30)
+    state_editor.grid(row=4, column=1)
+    p_code_editor = Entry(editor, width=30)
+    p_code_editor.grid(row=5, column=1)
+
+    f_name_label = Label(editor, text="First Name")
+    f_name_label.grid(row=0, column=0, padx=20, pady=(10, 0))
+    l_name_label = Label(editor, text="Last Name")
+    l_name_label.grid(row=1, column=0)
+    address_label = Label(editor, text="Address")
+    address_label.grid(row=2, column=0)
+    city_label = Label(editor, text="City")
+    city_label.grid(row=3, column=0)
+    state_label = Label(editor, text="County")
+    state_label.grid(row=4, column=0)
+    p_code_label = Label(editor, text="Post Code")
+    p_code_label.grid(row=5, column=0)
+
+    for record in records:
+        f_name_editor.insert(0, record[0])
+        l_name_editor.insert(0, record[1])
+        address_editor.insert(0, record[2])
+        city_editor.insert(0, record[3])
+        state_editor.insert(0, record[4])
+        p_code_editor.insert(0, record[5])
+
+    save_btn = Button(editor, text="Save Record", command=edit)
+    save_btn.grid(row=6, column=0, columnspan=2, padx=10, pady=10, ipadx=105)
+
 def delete():
     conn = sqlite3.connect('address_book.db')
     cur = conn.cursor()
@@ -60,7 +112,7 @@ def query():
         print_records += str(record[0]) + " " + str(record[1]) + " " + "\t" + str(record[6]) + "\n"
 
     query_label =Label(root, text=print_records)
-    query_label.grid(row=11, column=0, columnspan=2)
+    query_label.grid(row=15, column=0, columnspan=2)
 
     conn.commit()
     conn.close()
@@ -105,6 +157,9 @@ query_btn.grid(row=7, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
 
 delete_btn = Button(root, text="Delete Record", command=delete)
 delete_btn.grid(row=10, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
+
+edit_btn = Button(root, text="Edit Record", command=edit)
+edit_btn.grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=105)
 
 conn.commit()
 conn.close()
